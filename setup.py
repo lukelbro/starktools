4c781f1ae232d67e75941cc837a4c6f5c3af8e09
Luke Brown <luke.brown.19@ucl.ac.uk>
2021-10-22 16:33:43 +0100

    small changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="starktools",
-    version="0.1.5",
+    version="0.1.6",
     author="Luke Brown",
     packages=find_packages(exclude=['*tests']),
     install_requires=['numpy', 'numba']
