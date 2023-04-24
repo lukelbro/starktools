58e9dcc3edf5ee1679cacf2681ca1bb6524cde0d
Luke Brown <lukelbro@gmail.com>
2023-04-24 13:15:29 +0100

    version 0.1.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="starktools",
-    version="0.1.14",
+    version="0.1.15",
     author="Luke Brown",
     packages=find_packages(exclude=['*tests']),
     install_requires=['numpy', 'nested_dict']
